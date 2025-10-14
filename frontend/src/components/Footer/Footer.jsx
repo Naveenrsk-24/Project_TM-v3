912435ef7fb11bb9e144c315b25f521885b6443c
NaveenRsk <naveenrsk.dev@gmail.com>
2025-10-14 18:49:01 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -182,7 +182,7 @@ export default function Footer() {
 
         <hr className="mt-16 mb-10 border-gray-200" />
         <p className="text-sm text-center text-gray-600">
-          © Copyright 2021, All Rights Reserved by Postcraft
+          © Copyright 2021, All Rights Reserved by TM
         </p>
       </div>
     </section>
