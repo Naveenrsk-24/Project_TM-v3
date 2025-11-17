f8f8908a539f613811f399b2a54bfc220558b98d
NaveenRsk <naveenrsk.dev@gmail.com>
2025-11-18 01:41:18 +0530

    Tm studios- Coming Soon

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,23 +7,12 @@ export const metadata = {
 export default function ComingSoon() {
   return (
     <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
-        padding: "2rem",
-        background: "#f8f8f8",
-      }}
-    >
-      <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>
-        🚧 Website Under Construction
-      </h1>
-      <p style={{ fontSize: "1.2rem", color: "#555" }}>
-        We’re working on something amazing. Please check back soon.
-      </p>
-    </div>
+      className="
+        min-h-screen w-full bg-no-repeat bg-contain bg-black bg-center
+        bg-[url('/comingsoon/phone.avif')]
+        sm:bg-[url('/comingsoon/laptop.avif')]
+      "
+    />
   );
 }
+
