2f16cb4008b77607bc754a7466e8aa80b4d1479c
Naveenrsk-24 <naveenrsk.dev@gmail.com>
2026-01-22 22:54:26 +0530

    removed backend and starpi

--- backend/AdminPanel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.admin_login, name='admin-login'),
-    path('logout/', views.admin_logout, name='admin_logout'),
-    path('dashboard/', views.admin_dashboard, name='admin-dashboard'),
-]
--- backend/TM_photography/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('event-booking/', views.event_booking_create, name='event-booking-create'),
-]
